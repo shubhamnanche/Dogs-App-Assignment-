650d22aed1a5220ed145ab5dcb0555daed6cc5d9
Shubham Nanche <73213552+shubhamnanche@users.noreply.github.com>
2025-02-14 13:15:57 +0530

    Initial commit

---
+++ app/src/main/java/com/svg/dogsapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.svg.dogsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.svg.dogsapp.ui.theme.DogsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DogsAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
